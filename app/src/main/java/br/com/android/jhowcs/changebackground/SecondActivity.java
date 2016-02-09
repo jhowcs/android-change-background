758c419d9b65ceb0d21df72d8e680f0b553cbd34
jhowcs <jhowcs@gmail.com>
2016-02-09 20:48:32 -0200

    I made some changes in the second activity to work with fragments and allow the users change the background by themselves

--- app/src/main/java/br/com/android/jhowcs/changebackground/SecondActivity.java
+++ app/src/main/java/br/com/android/jhowcs/changebackground/SecondActivity.java
@@ -1,18 +1,41 @@
 package br.com.android.jhowcs.changebackground;
 
-import android.app.Activity;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import br.com.android.jhowcs.changebackground.fragments.FragmentFour;
+import br.com.android.jhowcs.changebackground.fragments.FragmentOne;
+import br.com.android.jhowcs.changebackground.fragments.FragmentThree;
+import br.com.android.jhowcs.changebackground.fragments.FragmentTwo;
 
 /**
  * Created by jonathan_campos on 05/02/2016.
  */
-public class SecondActivity extends Activity {
+public class SecondActivity extends FragmentActivity implements View.OnClickListener {
+
+    private Button btnFragment1;
+    private Button btnFragment2;
+    private Button btnFragment3;
+    private Button btnFragment4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        btnFragment1 = (Button) findViewById(R.id.btnFragment1);
+        btnFragment2 = (Button) findViewById(R.id.btnFragment2);
+        btnFragment3 = (Button) findViewById(R.id.btnFragment3);
+        btnFragment4 = (Button) findViewById(R.id.btnFragment4);
+
+        btnFragment1.setOnClickListener(this);
+        btnFragment2.setOnClickListener(this);
+        btnFragment3.setOnClickListener(this);
+        btnFragment4.setOnClickListener(this);
     }
 
     @Override
@@ -20,5 +43,28 @@ public class SecondActivity extends Activity {
         Log.i(getClass().getCanonicalName().toString(), "Second has started!");
 
         super.onStart();
+    }
+
+    @Override
+    public void onClick(View view) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        switch (view.getId()) {
+            case R.id.btnFragment1:
+                ft.add(R.id.second_activity_placeholder, new FragmentOne());
+                break;
+            case R.id.btnFragment2:
+                ft.replace(R.id.second_activity_placeholder, new FragmentTwo());
+                break;
+            case R.id.btnFragment3:
+                ft.replace(R.id.second_activity_placeholder, new FragmentThree());
+                break;
+            case R.id.btnFragment4:
+                ft.replace(R.id.second_activity_placeholder, new FragmentFour());
+                break;
+            default:
+        }
+
+        ft.commit();
     }
 }
---
+++ app/src/main/java/br/com/android/jhowcs/changebackground/fragments/FragmentThree.java
@@ -0,0 +1,21 @@
+package br.com.android.jhowcs.changebackground.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.android.jhowcs.changebackground.R;
+
+/**
+ * Created by jonathan_campos on 09/02/2016.
+ */
+public class FragmentThree extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
